e0d6e515a732cd3a1c76ef59031ed6a8fefa3545
Yugraj <yugrajjoshi185@gmail.com>
2026-03-16 12:44:27 +0530

    authentication and login backend

--- thoughtflow/accounts/views.py
+++ thoughtflow/accounts/views.py
@@ -3,7 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer
-
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
 
 @api_view(['POST'])
 def register_user(request):
@@ -15,3 +16,16 @@ def register_user(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        token,created =Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+    
+    return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
--- thoughtflow/thoughtflow/urls.py
+++ thoughtflow/thoughtflow/urls.py
@@ -23,4 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('posts.urls')),
+    path('api/', include('accounts.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
