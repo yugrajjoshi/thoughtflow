9cc6a314f08c060fd6a4670e23b68f0e59219d0d
Yugraj <yugrajjoshi185@gmail.com>
2026-03-20 23:23:14 +0530

    namv bar design

--- thoughtflow-frontend/src/pages/Authpage.jsx
+++ thoughtflow-frontend/src/pages/Authpage.jsx
@@ -1,100 +1,110 @@
-import  React , { useState } from 'react'; 
+import React, { useState } from 'react';
 import { Eye, EyeOff } from "lucide-react";
 
 function AuthPage() {
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        // Handle form submission logic here
-       fetch("http://127.0.0.1:8000/api/login/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            username: username,
-            password: password })
+  function handleSubmit(event) {
+    event.preventDefault();
+    // Handle form submission logic here
+    fetch("http://127.0.0.1:8000/api/login/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
     })
-    .then(response => response.json())
-
-    .then(data => {
-        if(data.token) {
-            // Store the token in local storage or state management
-            localStorage.setItem("token", data.token);
-            // Redirect to the dashboard or home page
-            window.location.href = "/home";
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.token) {
+          // Store the token in local storage or state management
+          localStorage.setItem("token", data.token);
+          // Redirect to the dashboard or home page
+          window.location.href = "/home";
         }
-    })
+      });
+  }
 
-    }
-
-    const [showPassword,setShowPassword] = useState(false);
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
-  <div className="flex items-center justify-center h-screen bg-black">
-
-    <div className=" flex items-center justify-around bg-black rounded-lg -mt-10 w-full h-screen">
+    <div className="flex items-center justify-center h-screen bg-black">
+      <div className=" flex items-center justify-around bg-black rounded-lg -mt-10 w-full h-screen">
+        <img
+          src="src/assets/logo.svg"
+          alt="Logo Image"
+          className="w-[30%] h-[50%] object-cover rounded-lg"
+        />
         <div className="bg-black rounded-lg justify-center items-center shadow-lg pt-3 w-full max-w-md h-[80%]">
-             
-          <div className= "bg-white flex flex-col items-center justify-center rounded-2xl p-4 w-full " >
-             <div className=" border w-full  rounded-2xl p-10 m-5" >
-                 <h3 className= "text-2xl font-bold text-black text-shadow-black " >WELCOME!</h3>
-                  <br />
-                  <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
-                    <label htmlFor="username" className="text-black text-left ">Username</label>
+          <div className="bg-white flex flex-col items-center justify-center rounded-2xl p-4 w-full ">
+            <div className=" border w-full  rounded-2xl p-10 m-5">
+              <h3 className="text-2xl font-bold text-black text-shadow-black ">WELCOME!</h3>
+              <br />
+              <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+                <label htmlFor="username" className="text-black text-left ">
+                  Username
+                </label>
+                <input
+                  id="username"
+                  type="text"
+                  placeholder="Username"
+                  className="w-full rounded-2xl p-3 bg-black text-white placeholder:text-gray-400 border border-black focus:outline-none focus:ring-2 focus:ring-black"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <div className="flex flex-col">
+                  <label htmlFor="password" className="text-black text-left">
+                    Password
+                  </label>
+
+                  <div className="relative">
                     <input
-                      id="username"
-                      type="text"
-                      placeholder="Username"
-                      className="w-full rounded-2xl p-3 bg-black text-white placeholder:text-gray-400 border border-black focus:outline-none focus:ring-2 focus:ring-black"
-                      value={username}
-                      onChange={(e) => setUsername(e.target.value)}
+                      id="password"
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Password"
+                      className="rounded-2xl p-3 w-full bg-black text-white placeholder:text-gray-400 border border-black focus:outline-none focus:ring-2 focus:ring-black pr-10"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
                     />
-                    <div className="flex flex-col">
-  <label htmlFor="password" className="text-black text-left">Password</label>
 
-  <div className="relative">
-    <input
-      id="password"
-      type={showPassword ? "text" : "password"}
-      placeholder="Password"
-      className="rounded-2xl p-3 w-full bg-black text-white placeholder:text-gray-400 border border-black focus:outline-none focus:ring-2 focus:ring-black pr-10"
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-    />
-
-    {/* Eye Button */}
-    <button
-      type="button"
-      onClick={() => setShowPassword(!showPassword)}
-      className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white"
-    >
-      {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-    </button>
-  </div>
-
-  {/* Forgot Password */}
-  <a href="#" className="text-blue-500 hover:underline text-sm self-end mt-1">
-    Forgot password?
-  </a>
-</div>
-                    <button type="submit" className="bg-gray-500 hover:bg-gray-700 transition duration-200 text-white font-bold py-2 px-4 rounded-2xl">
-                        Login
+                    {/* Eye Button */}
+                    <button
+                      type="button"
+                      onClick={() => setShowPassword(!showPassword)}
+                      className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white"
+                    >
+                      {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
                     </button>
-                  </form>
-                  <br />
-                  Dont have an account ? <a href="#" className="text-blue-500 hover:underline">Sign up</a>
-                  <button className="bg-black hover:bg-gray-800 transition duration-200 rounded-4xl w-[70%] mt-7  text-white font-bold  p-4 ">
-                        Sign in with Google
-                    </button>
-                   </div>
-               </div> 
+                  </div>
+
+                  {/* Forgot Password */}
+                  <a href="#" className="text-blue-500 hover:underline text-sm self-end mt-1">
+                    Forgot password?
+                  </a>
+                </div>
+                <button
+                  type="submit"
+                  className="bg-gray-500 hover:bg-gray-700 transition duration-200 text-white font-bold py-2 px-4 rounded-2xl"
+                >
+                  Login
+                </button>
+              </form>
+              <br />
+              <div className="text-center">
+                Dont have an account ? <a href="#" className="text-blue-500 hover:underline">Sign up</a>
+              </div>
+              <button className="bg-black hover:bg-gray-800 transition duration-200 rounded-4xl w-[70%] mt-7 text-white font-bold p-4 mx-auto block">
+                Sign in with Google
+              </button>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  </div>
-      
-      );
+  );
 }
+
 export default AuthPage;
--- thoughtflow-frontend/src/pages/Home.jsx
+++ thoughtflow-frontend/src/pages/Home.jsx
@@ -1,17 +1,85 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import  { House , UserRound, Search, Mail, LogOut } from "lucide-react";
+
 
 
 
 function Home() {
+  const [loginStatus, setLoginStatus] = useState(() => Boolean(localStorage.getItem("token")));
 
-  const token = localStorage.getItem("token");
-  if(!token){
-    window.location.href = "/";
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setLoginStatus(Boolean(token));
+
+    if (!token) {
+      window.location.href = "/";
+    }
+  }, []);
+
+  if (!loginStatus) {
+    return null;
+  }
+
+  // buttons controls
+  const[activeButton, setActiveButton] = useState("home");
+
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
   };
+  function handleLogout() {
+    localStorage.removeItem("token");
+    window.location.href = "/";
+  }
+
+
   return (
-    <main className="min-h-screen bg-linear-to-br from-slate-50 via-white to-cyan-50 p-6 md:p-10">
-     <div> hello home screen </div>
-      login successfull
+    <main className="bg-black w-full h-screen ">
+      <nav className="fixed top-0 left-0 h-screen w-[20%] bg-black border-r overflow-hidden">
+          <div className="flex top-0 self-center w-[35%]">
+            <img
+              src="src/assets/logo.svg"
+              alt="Logo Image"
+              className=" ml-1 mt-1 w-full h-full object-cover rounded-lg"
+      
+            />
+         </div><div className="flex flex-col gap-2 mt-5">
+            <button 
+              className={`text-white gap-6 text-2xl font-bold p-3 transition duration-300 ml-6 w-[70%] ${activeButton === "home" ? "bg-zinc-800/30 " : "hover:bg-zinc-800/30  "} rounded-4xl flex items-center`}
+              onClick={() => handleButtonClick("home")}
+            >
+             <House className="w-9 h-9" />Home
+           </button>
+           <button 
+             className={`text-white gap-6 text-2xl font-bold p-3 transition duration-300 ml-6 w-[70%] ${activeButton === "profile" ? "bg-zinc-800/30 " : "hover:bg-zinc-800/30 "} rounded-4xl flex items-center`}
+             onClick={() => handleButtonClick("profile")}
+           >
+             <UserRound className="w-9 h-9" />Profile
+           </button>
+           <button 
+             className={`text-white gap-6 text-2xl font-bold p-3 transition duration-300 ml-6 w-[70%] ${activeButton === "explore" ? "bg-zinc-800/30 " : "hover:bg-zinc-800/30 "} rounded-4xl flex items-center`}
+             onClick={() => handleButtonClick("explore")}
+           >
+             <Search className="w-9 h-9" />Explore
+           </button>
+           <button 
+             className={`text-white gap-6 text-2xl font-bold p-3 transition duration-300 ml-6 w-[70%] ${activeButton === "chats" ? "bg-zinc-800/30 " : "hover:bg-zinc-800/30 "} rounded-4xl flex items-center`}
+             onClick={() => handleButtonClick("chats")}
+           >
+             <Mail className="w-9 h-9" />Chats
+           </button>
+           </div>
+           <button className = "text-white gap-6 text-1xl font-bold p-3 transition duration-300 ml-6 w-[70%] hover:bg-zinc-800/30 hover:shadow-md  rounded-4xl  flex items-center" onClick={handleLogout}>
+             <LogOut className="w-5 h-5" />Logout
+           </button>
+      </nav>
+      <div className="ml-[20%] flex h-screen w-[80%] overflow-y-auto">
+        <div className="flex min-h-screen w-2/3 text-white border-white border-l border-r">
+          feed
+        </div>
+        <div className="flex min-h-screen w-1/3 text-white bg-black border">
+          search
+        </div>
+      </div>
     </main>
   );
 }
