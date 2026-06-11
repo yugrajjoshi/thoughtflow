fbebf916b3c4598cd920d11dcfba4c292c4caa9c
Yugraj <yugrajjoshi185@gmail.com>
2026-06-12 00:43:55 +0530

    add CSRF_TRUSTED_ORIGINS for Render production

--- thoughtflow/thoughtflow/settings.py
+++ thoughtflow/thoughtflow/settings.py
@@ -212,6 +212,12 @@ else:
         if host.strip()
     ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.onrender.com',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
