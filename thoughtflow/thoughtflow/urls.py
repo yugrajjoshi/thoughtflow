de4b6340b134b7ae48dd3fbf6d8b2ba40d8fcbe7
Yugraj <yugrajjoshi185@gmail.com>
2026-06-12 14:02:59 +0530

    enable media serving in production

--- thoughtflow/thoughtflow/urls.py
+++ thoughtflow/thoughtflow/urls.py
@@ -20,12 +20,15 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.urls import re_path
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('posts.urls')),
     path('api/', include('accounts.urls')),
     path('api/', include('chat.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    # Force Django to serve media files in production (DEBUG=False)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
