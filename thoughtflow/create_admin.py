4204e499b50d9d7428f3b14a7e7186d9cfd072e3
Yugraj <yugrajjoshi185@gmail.com>
2026-06-12 16:53:47 +0530

    update default superuser credentials to yuvi

--- thoughtflow/create_admin.py
+++ thoughtflow/create_admin.py
@@ -8,9 +8,9 @@ from django.contrib.auth import get_user_model
 
 def create_admin():
     User = get_user_model()
-    username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
-    email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-    password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'adminpassword')
+    username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'yuvi')
+    email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'yugrajjoshi37@gmail.com')
+    password = os.getenv('DJANGO_SUPERUSER_PASSWORD', '4660533')
 
     if not User.objects.filter(username=username).exists():
         User.objects.create_superuser(username=username, email=email, password=password)
